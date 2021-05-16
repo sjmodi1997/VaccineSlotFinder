d6f30db380bb72e2043102eacdba4eb792d7291e
Smit <sjmodi1997.sm@gmail.com>
2021-05-16 18:45:40 +0530

    Adding Configuration Params to find required slot

--- slotsFinder.py
+++ slotsFinder.py
@@ -7,8 +7,42 @@ from tkinter import *
   
 from tkinter import messagebox  
 
+#----------------------------------------Update the following Configuration as per req----------------------------------------
+
 #pincodes to check
 pincodes = ["380001","380022","380008"]
+
+#Set true if you are lookig for 1st Dose
+lookForDose1 = True
+#Set true if you are lookig for 2st Dose
+lookForDose2 = False
+
+#set true if you are looking for Covishield
+lookForCoviShield = True
+#set true if you are looking for Covaxin
+lookForCovaxin = True
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+checkDoseString = ""
+vaccineName = ""
+sleepSec = 3
+
+def init():
+    global checkDoseString, vaccineName
+    if lookForDose1 == True and lookForDose2 == True:
+        checkDoseString = "available_capacity"
+    elif lookForDose1 == True:
+        checkDoseString = "available_capacity_dose1"
+    else:
+        checkDoseString = "available_capacity_dose2"
+    
+    if lookForCovaxin == True and lookForCoviShield == True:
+        vaccineName = ""
+    elif lookForCoviShield == True:
+        vaccineName = "COVISHIELD"
+    else:
+        vaccineName = "COVAXIN"
 
 #generates alert if you find the matching slot
 def generateAlert(output):
@@ -34,20 +68,18 @@ def main():
         for center in my_map['centers']:
             #print(center)
             for session in center['sessions']:
-                #print(session)
-                if session['available_capacity_dose1'] > 0:
-                    output = pincode + " :: " + center['name'] + " :: " + session['date'] + " => "+ (str(session['available_capacity']))
-                    generateAlert(output)
-        print("Done!!")
-        sec = 15
-        print("Sleeping for " + str(sec) + " secs")
-        time.sleep(sec)
+                # print(session)
+                # print(checkDoseString)
+                # print(vaccineName)
+                if session[checkDoseString] > 0:
+                    if (not vaccineName) or (session['vaccine'] == vaccineName): 
+                        output = pincode + " :: " + center['name'] + " :: " + session['vaccine'] + " :: " + session['date'] + " => "+ (str(session['available_capacity']))
+                        generateAlert(output)
+        print("No Req Slots found for pincode :: " + pincode)
+        print("Sleeping for " + str(sleepSec) + " secs")
+        time.sleep(sleepSec)
 
 if __name__=="__main__":
-    #generateAlert("test")
-    sec = 60
+    init()
     while(True):
         main()
-        # print("Sleeping for " + str(sec) + " secs")
-        # time.sleep(sec)
-
