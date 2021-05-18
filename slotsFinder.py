5772ab229139cc35da8d9e8d6e1f88e4a6ac010f
Smit <sjmodi1997.sm@gmail.com>
2021-05-18 23:44:45 +0530

    Adding Random Agent to avoid banned from site

--- slotsFinder.py
+++ slotsFinder.py
@@ -4,8 +4,9 @@ import json
 import time
 from datetime import date 
 from tkinter import *  
-  
 from tkinter import messagebox  
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
 #----------------------------------------Update the following Configuration as per req----------------------------------------
 
@@ -26,7 +27,16 @@ lookForCovaxin = True
 
 checkDoseString = ""
 vaccineName = ""
-sleepSec = 3
+sleepSec = 5
+
+#method to get the random user agent to hit more different request
+def getRandomUserAgent():
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value] 
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+    #user_agents = user_agent_rotator.get_user_agents()
+    user_agent = user_agent_rotator.get_random_user_agent()
+    return user_agent
 
 def init():
     global checkDoseString, vaccineName
@@ -54,7 +64,8 @@ def main():
     today = date.today().strftime('%d-%m-%Y')
     for pincode in pincodes:
         URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=" + pincode + "&date=" + today
-        headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'}
+        random_user_agent = getRandomUserAgent()
+        headers = {"User-Agent": random_user_agent}
         page = requests.get(URL, headers=headers)
         soup = BeautifulSoup(page.content,'html.parser')
 
@@ -64,13 +75,11 @@ def main():
             my_map = json.loads(ret)
         except:
             print(ret)
+            print("Sleeping for " + str(sleepSec) + " secs")
+            time.sleep(sleepSec)
             continue
         for center in my_map['centers']:
-            #print(center)
             for session in center['sessions']:
-                # print(session)
-                # print(checkDoseString)
-                # print(vaccineName)
                 if session[checkDoseString] > 0:
                     if (not vaccineName) or (session['vaccine'] == vaccineName): 
                         output = pincode + " :: " + center['name'] + " :: " + session['vaccine'] + " :: " + session['date'] + " => "+ (str(session['available_capacity']))
